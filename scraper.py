199b09527c47700a2e2fea2c718271e88981b67e
chris48s <chris.shaw480@gmail.com>
2017-03-28 16:51:39 +0100

    use RandomIdArcGisScraper so we get stable hashes

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-from dc_base_scrapers.arcgis_scraper import ArcGisScraper
+from dc_base_scrapers.arcgis_scraper import RandomIdArcGisScraper
 
 
 stations_url = "https://mapping.canterbury.gov.uk/arcgis/rest/services/Open_Data/Polling_Stations/MapServer/0/query?where=OBJECTID+LIKE+%27%25%27&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=OBJECTID&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson"
@@ -6,7 +6,7 @@ districts_url = "https://mapping.canterbury.gov.uk/arcgis/rest/services/Open_Dat
 council_id = 'E07000106'
 
 
-stations_scraper = ArcGisScraper(stations_url, council_id, 'utf-8', 'stations')
+stations_scraper = RandomIdArcGisScraper(stations_url, council_id, 'utf-8', 'stations')
 stations_scraper.scrape()
-districts_scraper = ArcGisScraper(districts_url, council_id, 'utf-8', 'districts')
+districts_scraper = RandomIdArcGisScraper(districts_url, council_id, 'utf-8', 'districts')
 districts_scraper.scrape()
